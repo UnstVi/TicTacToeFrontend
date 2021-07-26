083a5a055ededbeda12ee6ad5cd682b30163dc10
UnstVi <49830123+UnstVi@users.noreply.github.com>
2021-07-26 19:45:47 +0300

    delete react create app unused files

---
+++ src/Components/Cog.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Cog = () => {
+    return (
+        <div>
+            <i className="fa fa-cog fa-spin" style={{fontSize: '35px', color:'#DF622D', marginLeft: '1%', marginTop: '1vh'}} ></i>
+        </div>
+    )
+}
+
+export default Cog
---
+++ src/Components/Menu.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useState } from 'react'
+
+
+const Menu = () => {
+    
+ 
+    return (
+        <div className="menu">
+
+            <h1 className="play">play</h1>
+
+        </div>
+    )
+}
+
+export default Menu
